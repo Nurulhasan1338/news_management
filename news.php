9415cd645e6c1d87ba76ea0e605a8ebf651aedb6
Nurul Hasan <2020kucp1109@iiitkota.ac.in>
2023-06-26 12:38:30 +0530

    modified news.php

--- news.php
+++ news.php
@@ -23,65 +23,48 @@
         
     .headimg{
         border-radius: 50%;
-        width: 80%;
+        width: 15%;
 
     }
     .headline{
         display: flex;
-        align-items: center;
-        font-family: 'Noto Sans Devanagari', sans-serif;
-        text-shadow: -5px 4px 7px #3f3f3f;
-        font-size:60px;
-        color: black;
+        background-color: white;
         
     }
-    /* .headline::after{
-        content: "खबरों का नया ज़रिया";
-        
-    position: relative;
-    top: 1.6rem;
-    left: -10rem;
-    font-size: 21px;
-    color: #0a0a9b;
-        
-    } */
+   .r-menu{
+    background-color: white;
+    
+   }
+   a{
+    text-decoration: none;
+   }
 
     .heading{
         height: auto
     }
     @media (max-width: 1000px)  {
         .headline{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 30px;
-        font-size: 5rem;
+      
 
-        
+      
     }
     .headimg{
         border-radius: 50%;
-        width: 30%;
 
     }
     .heading{
         height: auto;
-        margin-top: 40px;
     }
     }
 
     @media (max-width: 550px){
         .headline{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-       
-        margin-top: 10px;
-        font-size: 3rem;
-        width: 100%;
-        text-align: center;
+        visibility:hidden;
         
     }
+    .r-menu{
+    
+   }
     .content{
         font-size: 32px;
     }
@@ -115,19 +98,26 @@
           </div>
         </div>
         <div class="col-lg-10">
-            <div class="row p-lg-3 heading">
-                <div class= "col-lg-2 col-md-1 d-flex justify-content-center"><img src="./image/img.jpg" class="headimg" alt="country vision" ></div>
-                <div class="col headline"><p>कंट्री विजन</p></div>
+            <div class="row heading">
+                <div class="col d-flex justify-content-center headline"><img src="./image/img.jpg" class="headimg" alt="country vision" ></div>
             </div>
-            <div class="row bg-dark text-light">
-                <div class="col text-light"><a href="profile.php" class="menu-link">catigory</a> </div>
-                <div class="col text-light"><a href="profile.php" class="menu-link">option 1</a> </div>
-                <div class="col text-light"><a href="profile.php" class="menu-link">option 2</a> </div>
+            <div class="row r-menu pb-3 ">
+                <div class="col d-flex justify-content-center fw-bolder">
+                    <div class="mx-4 px-3"><a href="profile.php">option 3</a> </div>
+                    <div class="mx-4 px-3"><a href="profile.php">option 1</a> </div>
+                    <div class="mx-4 px-3"><a href="profile.php">option 2</a> </div>
+                </div>
             </div>
             <div class="row p-3">
-
-                <div class="col content">
+                <div class="col-12 content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis tempora dignissimos quas praesentium aliquam exercitationem optio earum adipisci porro cum! A beatae, ea natus reprehenderit explicabo dolorum voluptatem tenetur, sunt possimus facere quasi nihil saepe sapiente, accusamus minus ipsam error? Accusantium eum autem tempore ad incidunt doloremque mollitia, commodi eius animi officia deleniti. Quaerat commodi, dolor officia voluptate perferendis debitis hic laudantium doloribus numquam cupiditate nihil ipsa earum distinctio. Hic veritatis qui ea alias! Minima dolor blanditiis at veritatis doloremque eaque esse vitae  aliquid reprehenderit voluptatibus placeat quod? Aliquid saepe obcaecati nemo id corrupti expedita inventore libero sequi aperiam nostrum, voluptate iusto. Ipsam itaque in placeat, sequi doloribus sunt impedit culpa, ut repellendus eum facere odit recusandae. Libero rem, officia maxime amet adipisci soluta temporibus corrupti consequuntur culpa provident, odit, dolore perferendis optio suscipit laborum earum! At, ipsa asperiores. Deleniti labore, nihil adipisci voluptatibus, quaerat alias eligendi enim natus corporis rem quia vero blanditiis deserunt fugiat. Ipsa recusandae repudiandae harum incidunt reprehenderit rerum ipsam, corporis consectetur ex beatae cumque vitae necessitatibus officiis minima, aliquid corrupti voluptatibus consequuntur veniam nemo? Explicabo, asperiores? Deserunt perferendis aliquid voluptatibus consectetur reiciendis, a corporis id exercitationem voluptatem! Fuga voluptas iusto, blanditiis vero repellat dolor vel veniam tempora rem accusamus expedita totam reiciendis praesentium deleniti. Officiis non quod adipisci tempore debitis labore soluta sapiente vitae consequuntur, architecto ipsam tempora repellendus maiores commodi itaque sit inventore, quos eius? Ducimus, dolorem quae? Sequi atque quisquam laborum molestiae a laudantium in! Aliquid quisquam facilis ratione repellendus esse? Quia, quis sunt. Sequi, nihil vitae, qui dolor dolore reiciendis deserunt, est autem omnis tenetur fugit ipsa blanditiis ipsum alias culpa harum quia quas. Doloribus, nostrum. Qui eius deleniti tempore recusandae nam amet distinctio. Expedita praesentium quia, provident eligendi, odit obcaecati incidunt laborum cum modi eum debitis! Commodi rerum odio illum consequatur quasi repellat similique suscipit architecto nisi neque modi odit deleniti aliquid necessitatibus illo officiis, distinctio vitae atque quidem, error dolore ullam sit. Quisquam ad quod incidunt aliquid dignissimos, atque necessitatibus in aperiam modi. Necessitatibus enim laboriosam aliquid adipisci magnam. Iste, culpa amet, dolores voluptatum, saepe deserunt nesciunt laboriosam quisquam ipsa itaque nemo quia. Nostrum, fugiat explicabo voluptatibus eveniet id odit, quisquam sequi aut natus totam dignissimos numquam cumque quis pariatur, consequuntur architecto obcaecati. Nostrum itaque, at dolore rerum quam voluptatum eaque voluptas culpa dolorem veritatis in soluta molestias ea reiciendis cum! Pariatur, distinctio! Sit iure qui aliquid expedita voluptatum accusamus veniam ipsam sapiente quos reprehenderit! Dicta magni non excepturi. Voluptates deserunt esse quasi soluta commodi maiores architecto voluptatum vitae incidunt, nam sint officia, unde dolor quae dolores rem, ipsum ea corporis ipsam. Praesentium quibusdam possimus maxime ea totam optio inventore, neque vero, culpa quis ducimus libero eligendi quidem sapiente eaque maiores suscipit? Esse nobis vitae dolor est laboriosam. Error quas reiciendis ab odit, debitis distinctio magni consectetur deleniti earum, vero cum ea, recusandae possimus culpa. Est aliquam consectetur distinctio eveniet, libero laboriosam minus in nostrum nemo minima praesentium pariatur quae hic nisi, veritatis vero nam voluptates dolor reprehenderit vel optio possimus nesciunt velit. Inventore in quod numquam cum voluptatem repellendus facere. Est debitis architecto neque provident eligendi magnam vel, optio adipisci incidunt, sed cupiditate ipsam non quis molestiae, eveniet dignissimos maiores natus quas. Nam, eos, cum commodi minima praesentium numquam tempore fuga aliquam itaque temporibus molestiae ex aspernatur vitae eaque in. Aliquam, incidunt provident! Eos tempora cum reiciendis. Distinctio, ab, cum ipsam officia sunt facere dolorum adipisci enim possimus omnis quaerat! Alias, quos unde.
+                </div>
+                <div class="col">
+                    <label for="" class="display-3 pt-4">Downlode</label>
+                    <div class="btn-group mx-3 pb-4 my-sm-4" role="group" aria-label="Basic mixed styles example">
+                        <button type="button" class="btn btn-info">Photo</button>
+                        <button type="button" class="btn btn-dark">Vedio</button>
+                      </div>
                 </div>
             </div>
                        
